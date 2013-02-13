a3e23fc5e4b0f3e1586d5185019a9c84482ec9de
Shuhei Nagata <shnagata.41@gmail.com>
2013-02-13 14:32:34 +0900

    お題その4

--- average_calcurator.rb
+++ average_calcurator.rb
@@ -1,11 +1,13 @@
 class AvarageCalcurator
   def calc(appearances, at_bat, hits)
-    if appearances == 0
-      nil
-    elsif at_bat == 0
-      0.000
+    if appearances == 0 || at_bat == 0
+      "---"
+    elsif hits == 0
+      ".000"
+    elsif at_bat == hits
+      "1.00"
     else
-      (hits.to_f / at_bat).round(3)
+      "." + "#{((hits.to_f / at_bat).round(3)*1000).to_i}"
     end
   end
 end
--- spec/average_calcurator_spec.rb
+++ spec/average_calcurator_spec.rb
@@ -6,28 +6,40 @@ describe AvarageCalcurator do
   describe "#calc" do
     let(:calcurator) { AvarageCalcurator.new }
 
-    it "10打席、8打数、4安打の時、0.5を返す" do
-      calcurator.calc(10, 8, 4).should eq 0.5
+    it "10打席、8打数、4安打の時、.500を返す" do
+      calcurator.calc(10, 8, 4).should eq ".500"
     end
 
-    it "10打席、10打数、4安打の時、0.4を返す" do
-      calcurator.calc(10, 10, 4).should eq 0.4
+    it "10打席、10打数、4安打の時、.400を返す" do
+      calcurator.calc(10, 10, 4).should eq ".400"
     end
 
-    it "10000打席、10000打数、1234安打の時、0.123を返す" do
-      calcurator.calc(10000, 10000, 1234).should eq 0.123
+    it "10000打席、10000打数、1234安打の時、.123を返す" do
+      calcurator.calc(10000, 10000, 1234).should eq ".123"
     end
 
-    it "10000打席、10000打数、1235安打の時、0.124を返す" do
-      calcurator.calc(10000, 10000, 1235).should eq 0.124
+    it "10000打席、10000打数、1235安打の時、.124を返す" do
+      calcurator.calc(10000, 10000, 1235).should eq ".124"
     end
 
-    it "0打席、0打数、0安打の時、nilを返す" do
-      calcurator.calc(0, 0, 0).should eq nil
+    it "0打席、0打数、0安打の時、---を返す" do
+      calcurator.calc(0, 0, 0).should eq "---"
     end
 
-    it "10打席、0打数、0安打の時、0.000を返す" do
-      calcurator.calc(10, 0, 0).should eq 0.000
+    it "10打席、1打数、0安打の時、.000を返す" do
+      calcurator.calc(10, 1, 0).should eq ".000"
+    end
+
+    it "10打席、9打数、3安打の時、.333を返す" do
+      calcurator.calc(10, 9, 3).should eq ".333"
+    end
+
+    it "10打席、10打数、10安打の時、1.00を返す" do
+      calcurator.calc(10, 10, 10).should eq "1.00"
+    end
+
+    it "10打席、0打数、0安打の時、---を返す" do
+      calcurator.calc(10, 0, 0).should eq "---"
     end
   end
 end
