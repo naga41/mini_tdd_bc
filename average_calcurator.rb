df3f947dd376786d2a6beb55f9d4b3d0b6e864ec
Shuhei Nagata <shnagata.41@gmail.com>
2013-02-13 15:47:29 +0900

    BaseballPlayerクラスに変更

--- average_calcurator.rb
+++ average_calcurator.rb
@@ -1,13 +1,22 @@
-class AvarageCalcurator
-  def calc(appearances, at_bat, hits)
-    if appearances == 0 || at_bat == 0
+class BaseballPlayer
+  def initialize(appearances, at_bat, hits)
+    @appearances = appearances
+    @at_bat      = at_bat
+    @hits        = hits
+  end
+
+  def calc
+    if @appearances == 0 || @at_bat == 0
       "---"
-    elsif hits == 0
+    elsif @hits == 0
       ".000"
-    elsif at_bat == hits
+    elsif @at_bat == @hits
       "1.00"
     else
-      "." + "#{((hits.to_f / at_bat).round(3)*1000).to_i}"
+      "." + "#{((@hits.to_f / @at_bat).round(3)*1000).to_i}"
     end
+  end
+
+  def ranking(players)
   end
 end
--- spec/average_calcurator_spec.rb
+++ spec/average_calcurator_spec.rb
@@ -2,44 +2,52 @@
 require File.dirname(__FILE__) + "/spec_helper"
 require "average_calcurator"
 
-describe AvarageCalcurator do
+describe BaseballPlayer do
   describe "#calc" do
-    let(:calcurator) { AvarageCalcurator.new }
+    let(:player) { BaseballPlayer.new }
 
     it "10打席、8打数、4安打の時、.500を返す" do
-      calcurator.calc(10, 8, 4).should eq ".500"
+      BaseballPlayer.new(10, 8, 4).calc.should eq ".500"
     end
 
     it "10打席、10打数、4安打の時、.400を返す" do
-      calcurator.calc(10, 10, 4).should eq ".400"
+      BaseballPlayer.new(10, 10, 4).calc.should eq ".400"
     end
 
     it "10000打席、10000打数、1234安打の時、.123を返す" do
-      calcurator.calc(10000, 10000, 1234).should eq ".123"
+      BaseballPlayer.new(10000, 10000, 1234).calc.should eq ".123"
     end
 
     it "10000打席、10000打数、1235安打の時、.124を返す" do
-      calcurator.calc(10000, 10000, 1235).should eq ".124"
+      BaseballPlayer.new(10000, 10000, 1235).calc.should eq ".124"
     end
 
     it "0打席、0打数、0安打の時、---を返す" do
-      calcurator.calc(0, 0, 0).should eq "---"
+      BaseballPlayer.new(0, 0, 0).calc.should eq "---"
     end
 
     it "10打席、1打数、0安打の時、.000を返す" do
-      calcurator.calc(10, 1, 0).should eq ".000"
+      BaseballPlayer.new(10, 1, 0).calc.should eq ".000"
     end
 
     it "10打席、9打数、3安打の時、.333を返す" do
-      calcurator.calc(10, 9, 3).should eq ".333"
+      BaseballPlayer.new(10, 9, 3).calc.should eq ".333"
     end
 
     it "10打席、10打数、10安打の時、1.00を返す" do
-      calcurator.calc(10, 10, 10).should eq "1.00"
+      BaseballPlayer.new(10, 10, 10).calc.should eq "1.00"
     end
 
     it "10打席、0打数、0安打の時、---を返す" do
-      calcurator.calc(10, 0, 0).should eq "---"
+      BaseballPlayer.new(10, 0, 0).calc.should eq "---"
     end
+
+    # it "選手のリストを受け取り、打率の順位を返す" do
+    #   ebis = BaseballPlayer.new(10, 10, 10)
+    #   ktwk = BaseballPlayer.new(10, 10, 5)
+    #   ngt  = BaseballPlayer.new(0, 0, 0)
+    #   nskw = BaseballPlayer.new(10, 0, 0)
+    #   player.ranking([ebis, ktwk, ngt, nskw]).should eq ["ebis", "ktwk", "nskw", "ngt"]
+    # end
   end
 end
